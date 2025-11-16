c9fb3fa8441b577866991a9458f46a8f036f2b15
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-11-16 17:51:49 +0500

    testing +=

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     String s1 = "Hey this is Maaz", s2 = s1, s3;
-    s3 = "\nThis is a separate string";
+    s3 = "\nThis is a separate string\n";
+    s3+=s2;
     std::cout << (s1 + "\n" + s2 + s3);
 }
